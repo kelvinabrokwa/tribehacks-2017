1dfbdb4d351cb16a0b678f9a54b6e02db604f5bf
kelvinabrokwa <kelvinabrokwa@gmail.com>
2017-04-01 19:21:31 -0400

    more logging

--- central.js
+++ central.js
@@ -5,14 +5,26 @@
 var noble = require('noble');
 var express = require('express');
 var cors = require('cors');
+var morgan = require('morgan');
 
 var app = express();
 app.use(cors());
+app.use(morgan('combined'));
 
 //
 // BLE stuff
 //
-var bleServices = {};
+var bleData = {};
+
+var bleCharacteristics = {};
+
+var characteristicNames = {
+  lockOpenClose: '33b698449c9d4493a1029ca17db2d5b7'
+};
+
+var serviceNames = {
+  lockOpenClose: '9d0c027c9ed148e984f3f59dd2a204b2'
+};
 
 noble.on('stateChange', state => {
   if (state === 'poweredOn') {
@@ -46,9 +58,9 @@ noble.on('discover', peripheral => {
       console.log('discovered ' + services.length + ' services');
 
       for (var i = 0; i < services.length; i++) {
-
-        bleServices[services[i].uuid] = {};
-
+        console.log('new service: ' + services[i].uuid);
+        bleData[services[i].uuid] = {};
+        bleCharacteristics[services[i].uuid] = {};
         discoverCharacteristics(services[i]);
       }
     });
@@ -64,6 +76,9 @@ function discoverCharacteristics(service) {
     console.log('discovered ' + characteristics.length + ' characteristics');
 
     for (var j = 0; j < characteristics.length; j++) {
+      console.log('new characteristic\n\tservice: ' + service.uuid + '\n\tcharacteristic: ' +
+        characteristics[j].uuid);
+      bleCharacteristics[service.uuid][characteristics[j].uuid] = characteristics[j];
       setDataListener(service, characteristics[j]);
       characteristics[j].subscribe();
     }
@@ -72,7 +87,7 @@ function discoverCharacteristics(service) {
 
 function setDataListener(service, characteristic) {
   characteristic.on('data', (data, isNotification) => {
-    bleServices[service.uuid][characteristic.uuid] = data.readInt32LE(0);
+    bleData[service.uuid][characteristic.uuid] = data.readInt32LE(0);
   });
 }
 
@@ -84,7 +99,30 @@ app.get('/', (req, res) => {
 });
 
 app.get('/data', (req, res) => {
-  res.json(bleServices);
+  res.json(bleData);
+});
+
+app.post('/devices/lock/lock', (req, res) => {
+  var buf = Buffer.allocUnsafe(8);
+  buf.writeInt32LE(1);
+  var characteristic = bleCharacteristics[serviceNames['lockOpenClose']][characteristicNames['lockOpenClose']];
+  characteristic.once('write', () => {
+    console.log('writing 1 to\n\tservice:' + serviceNames['lockOpenClose'] +
+      '\n\tcharacteristic: ' + characteristicNames['lockOpenClose']);
+  });
+  characteristic.read((err, data) => {
+    if (err) {
+      return console.log(err);
+    }
+
+  });
+  characteristic.write(buf, false, err => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send('ok');
+    }
+  });
 });
 
 app.listen(8080, () => {
