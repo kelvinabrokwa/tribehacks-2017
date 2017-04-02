28487e9f3a3092e7cda08b3f93a9081253c23be2
kelvinabrokwa <kelvinabrokwa@gmail.com>
2017-04-01 23:53:30 -0400

    more frontend

--- index.js
+++ index.js
@@ -7,15 +7,12 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
-    return (<div>
-      <div>
-        <div>temperature</div>
-        <div>light</div>
-      </div>
-      <div>
-        <div>noise</div>
-        <div>lock</div>
-      </div>
+    return (<div className='container full-height full-width white big center'>
+      <div className='widget half-width half-height bg-purple pad2'>temp</div>
+      <div className='widget half-width half-height bg-orange pad2'>light</div>
+      <div className='widget third-width half-height bg-gray pad2'>lock</div>
+      <div className='widget third-width half-height bg-green pad2'>noise</div>
+      <div className='widget third-width half-height bg-blue pad2'>lock</div>
     </div>);
   }
 }
